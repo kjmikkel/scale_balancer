120bcd9e36527002bbcd09e95a6379f5f174e324
Mikkel Kjaer Jensen <kjmikkel@gmail.com>
2014-07-20 12:16:35 +0200

    First draft of a working solution written in oCaml

--- solver/scale_balancer_solver.ml
+++ solver/scale_balancer_solver.ml
@@ -2,17 +2,29 @@ let rec print_list = function
   [] -> ()
   | e::l -> print_int e ; print_string " " ; print_list l
 
-let tuple_decode (n, elem) = 
-  let rec add_one_elem i acc = 
-      match i  with
-      | 0 -> acc
-      | i -> add_one_elem (i-1) (elem::acc)
-  in add_one_elem n [] 
+let calc_total_weight weight cert =
+  match (weight, cert) with (w1, w2), (c1, c2) -> c1 * w1 + c2 * w2
 
+let tuple_to_list (a, b)  = a :: b :: [];;
+
+let print_certificates (cert_1, cert_2) weight_1 weight_2 =
+  match (cert_2, weight_1, weight_2) with (c3, c4), (w1, w2), (w3, w4) ->
+    print_string "Certificate 1:\n" ; 
+    print_list cert_1 ; 
+    print_string "\nWeight:\n" ; print_list (w1 :: w2 :: []) ;
+    print_string "\n\nCertificate 2:\n" ; print_list (c3 :: c4 :: []) ; 
+    print_string "\nWeight:\n" ; print_list (w3 :: w4 :: []) ; 
+    print_string "\n\nTotal weight: " ; print_int (calc_total_weight weight_2 cert_2) ; 
+    print_string "\n" ;; 
+   
+
+
+
+(* Given a compare weight and two types of weight, the system figures out how many of each are need (if it can be solved) to weigh the same as the compare value  *)
 let rec equal_compare weight cert compare =
   match (weight, cert, compare) with
-  |         _, (c1, c2), 0       -> c1 :: c2 :: []
-  |  (w1, w2), (c1, c2), compare -> 
+  |        _, (c1, c2), 0       -> c1 :: c2 :: []
+  | (w1, w2), (c1, c2), compare -> 
   let first_smallest = w1 < w2 in
   let smallest = if first_smallest then w1 else w2 in 
   let biggest  = if first_smallest then w2 else w1 in
@@ -37,16 +49,35 @@ let rec equal_compare weight cert compare =
                               else equal_compare weight (c1, c2 + 1) (compare - smallest);;
 
 
-let scale_balancer_aux weight_1 weight_2 cert_1 cert_2 =
-  match (weight_1, weight_2, cert_1, cert_2) with
-  | ((w1, w2), (w3, w4), (0, 0), (0, 0)) -> 1 :: 2 :: 3 :: 4 :: []
-  | ((w1, w2), (w3, w4), (c1, c2), (c3, c4)) -> 
-  if (w1 * c1 + w2 * c2) = (w3 * c3 + w4 * c4) then c1 :: c2 :: c3 :: c4 :: [] 
-                                               else 2 :: 3 :: 4 :: 5 :: [] ;;
+let increase_capacity weight cert =
+  match (weight, cert) with
+  | ((w1, w2), (c1, c2)) -> 
+  
+  let first_smallest = w1 < w2 in
+  let biggest = if first_smallest then w2 else w1 in
+  
+  (* We find out if the increase in the smallest will go over the biggest number *)
+  let next_smallest_total = if first_smallest then w1 * (c1 + 1) else c2 * (w2 + 1) in
+  let bigger_than_biggest = biggest < next_smallest_total in
+  
+  (* We figure out the new certificate  *)
+  let new_cert = if bigger_than_biggest then if first_smallest then (0, c2 + 1)
+                                                               else (c1 + 1, 0)
+                                        else if first_smallest then (c1 + 1, c2)
+                                                               else (c1, c2 + 1) in
+  (* return the new certificate *)
+  new_cert ;;
 
-let scale_balancer weight_1 weight_2 =
-  scale_balancer_aux weight_1 weight_2 (0, 0) (0, 0);;
+let scale_balance weight_1 weight_2 =
+  let rec scale_balance_aux weight_1 weight_2 cert =
+    let new_cert = increase_capacity weight_2 cert in
+    let weight_1_cert = equal_compare weight_1 (0, 0) (calc_total_weight weight_2 new_cert) in
+    if weight_1_cert == [] then scale_balance_aux weight_1 weight_2 new_cert
+                    else (weight_1_cert, new_cert)  
+  in 
+  scale_balance_aux weight_1 weight_2 (0, 0);;
 
-(* print_list (scale_balancer (1, 3) (2,4)) ;; *)
-
-print_list (equal_compare (7, 11) (0, 0) 22) ;;
+let weight_1 = (21, 105) in
+let weight_2 = (13, 9) in
+let result = scale_balance weight_1 weight_2 in
+print_certificates result weight_1 weight_2 ;;
